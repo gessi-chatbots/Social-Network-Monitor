c9be6684209fd36d813f86c78d10c214b27f893c
lucas.ares <lucas.ares@estudiantat.upc.edu>
2024-06-12 21:09:24 +0200

    urls updated

--- snmapp/api/views.py
+++ snmapp/api/views.py
@@ -280,7 +280,7 @@ class DeleteDocumentView(APIView):
             document.delete()
             return Response({'message': f'Document with identifier: {identifier} deleted successfully.'}, status=status.HTTP_200_OK)
         except Document.DoesNotExist:
-            return Response({'error': 'Document with identifier: {identifier} not found.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'Document not found.'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
   

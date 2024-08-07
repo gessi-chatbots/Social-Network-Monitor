aee3e521bfdd0e8cf816ca843e9307c1728717c8
mtiessler <max.tiessler@upc.edu>
2024-08-07 09:30:27 +0200

    added mysql library, changed model to fit in mysql

--- snmapp/models.py
+++ snmapp/models.py
@@ -3,18 +3,18 @@ import uuid
 
 
 class Document(models.Model):
-    identifier = models.UUIDField(default=uuid.uuid4 , unique=True, max_length=500)
+    identifier = models.UUIDField(default=uuid.uuid4, unique=True)
     text = models.TextField()
     datePublished = models.DateField()
     dateCreated = models.DateField(auto_now_add=True)
-    url = models.URLField(unique=True, max_length=1000)
-    author = models.TextField(blank=True, null=True, max_length=500)
-    alternateName = models.CharField(blank=True, null=True, max_length=500)
-    additionalType = models.CharField(blank=True, null=True, max_length=500)
-    
+    url = models.URLField(unique=True, max_length=255)
+    author = models.TextField(blank=True, null=True)
+    alternateName = models.CharField(blank=True, null=True, max_length=255)
+    additionalType = models.CharField(blank=True, null=True, max_length=255)
+
     class Meta:
         constraints = [
-            models.CheckConstraint(check=models.Q(text__isnull=False) & models.Q(datePublished__isnull=False) & models.Q(url__isnull=False), name='required_fields')
+            models.CheckConstraint(
+                check=models.Q(text__isnull=False) & models.Q(datePublished__isnull=False) & models.Q(
+                    url__isnull=False), name='required_fields')
         ]
-
-
